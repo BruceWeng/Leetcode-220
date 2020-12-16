39312c148a6c140d16645b3c78cd654b1f5e7de9
Bruce Weng <weng8916@gmail.com>
2020-12-15 17:16:57 -0800

    384 update

--- 384. Shuffle an Array.js
+++ 384. Shuffle an Array.js
@@ -68,3 +68,46 @@ class Solution {
     [ arr[i], arr[j] ] = [ arr[j], arr[i] ];
   }
 }
+
+/**
+ * 2020/12/15 Update
+ * The easier to understand approach.
+ * @param {number[]} nums 
+ */
+/**
+ * @param {number[]} nums
+ */
+var Solution = function(nums) {
+  this.origin = nums;
+};
+
+/**
+ * Resets the array to its original configuration and return it.
+ * @return {number[]}
+ */
+Solution.prototype.reset = function() {
+  return this.origin;
+};
+
+/**
+ * Returns a random shuffling of the array.
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function() {
+  let result = [...this.origin];
+  for (let i = result.length-1; i > 0; i--) {
+    this.swap(result, i, Math.floor(Math.random() * (i+1)));
+  }
+  return result;
+};
+
+Solution.prototype.swap = function(array, a, b) {
+  [array[a], array[b]] = [array[b], array[a]];
+}
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
