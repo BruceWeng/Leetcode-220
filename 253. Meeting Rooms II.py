c393ef1e2944865a06c5bb2c494b465f995257ac
Bruce Weng <weng8916@gmail.com>
2020-12-21 10:48:03 -0800

    add greedy sol

--- 253. Meeting Rooms II.py
+++ 253. Meeting Rooms II.py
@@ -48,3 +48,29 @@ if __name__=="__main__":
     interval2 = Interval(5, 10)
     interval3 = Interval(15, 20)
     print(minMeetingRooms([interval1, interval2, interval3])) # 2
+
+'''
+Greedy solution: O(nlogn)
+
+public class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+        for(int i=0; i<intervals.length; i++) {
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int rooms = 0;
+        int endsItr = 0;
+        for(int i=0; i<starts.length; i++) {
+            if(starts[i]<ends[endsItr])
+                rooms++;
+            else
+                endsItr++;
+        }
+        return rooms;
+    }
+}
+'''
