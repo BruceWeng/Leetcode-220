cf2b280cb560fb19295a9f80185523d4ec28fb7f
Bruce Weng <weng8916@gmail.com>
2021-01-07 09:24:20 -0800

    update 560

--- 560. Subarray Sum Equals K.js
+++ 560. Subarray Sum Equals K.js
@@ -6,99 +6,6 @@ Note:
 The length of the array is in range [1, 20,000].
 The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
  */
-/**
- * Algorithm: PrefixSum + 2 Sum using Hash
- * 1. Create prefix sum by hashMap<sum[0, i-1], count>
- * 2. sum[i, j] = sum[0, j] - sum[0, i-1] --> sum[0, i-1] = sum[0, j] - sum[i, j]
- *        k          sum      hashMap-key     hashMap-key      sum          k
- * 3. Why don't map.put(sum[0, i - 1], 1) every time ?
-        if all numbers are positive, this is fine
-        if there exists negative number, there could be preSum count > 1
- */
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-const subarraySum = function(nums, k) {
-  if (nums.length === 0) return 0;
-
-  let map = new Map();
-  map.getOrDefault = function(key, defaultVal) {
-    if (map.has(key)) return map.get(key);
-    else map.set(key, defaultVal);
-    return defaultVal;
-  }
-
-  let sum = 0;
-  let result = 0;
-  map.set(0, 1);
-  
-  for (num of nums) {
-    sum += num;
-    if (map.has(sum - k)) {// there exist a key, that [hashmap-key = sum - k]
-      result += map.get(sum - k);
-    }
-    map.set(sum, map.getOrDefault(sum, 0) + 1);
-  }
-  return result;
-};
-
-/**
- * Leetcode Fundamental: 11/8 Update
- * Failure:
- * 1. Fail to think of using hashmap storing count
- * 2. Fail to use only one variable preSum to store prefix sum(rather than prefixSum[])
- * 
- * T: O(n)
- * S: O(n)
- */
-const subarraySum = (nums, k) => {
-  if (nums === undefined || nums.length === 0 || k === undefined) return 0;
-
-  let map = {
-    0: 1 // key: preSum, value: count
-  }; 
-
-  let preSum = 0;
-  let result = 0;
-  for (let num of nums) {
-    preSum += num;
-    // Update result
-    if ((preSum - k) in map) result += map[preSum - k];
-    // Update map
-    if (!(preSum in map)) map[preSum] = 1;
-    else map[preSum] += 1;
-  }
-
-  return result;
-};
-
-/**
- * Leetcode Fundamental: 12/12 Revisited
- * Brute Force: Iterate from 0 to n-1 to find sum of subarray == k and update result
- * 
- * T: O(n^3)
- * S: O(1)
- * 
- * Runtime: TLE
- * 
- */
-const subarraySum = (nums, k) => {
-  if (nums === undefined || nums.length === 0 || k === undefined) return 0;
-  
-  let n = nums.length;
-  let result = 0;
-  for (let i = 0; i < n; i += 1) { // start index of subarray
-    for (let j = i+1; j <= n; j += 1) { // end index of subarray (exclude!!!)
-      let sum = 0;
-      for (let k = i; k < j; k += 1) sum += nums[k];
-      if (sum === k) result += 1;
-    }
-  }
-
-  return result;
-};
 
 /**
  * Reduced Brute Force
@@ -108,50 +15,57 @@ const subarraySum = (nums, k) => {
  * S: O(1)
  * Runtime: 304 ms
  */
-const subarraySum = (nums, k) => {
-  if (nums === undefined || nums.length === 0 || k === undefined) return 0;
+// const subarraySum = (nums, k) => {
+//   if (nums === undefined || nums.length === 0 || k === undefined) return 0;
   
-  let n = nums.length;
-  let result = 0;
-  for (let i = 0; i < n; i += 1) { // start index of subarray
-    let sum = 0;
-    for (let j = i; j < n; j += 1) { // end index of subarray (include!!!)
-      sum += nums[j];
-      if (sum === k) result += 1;
-    }
-  }
+//   let n = nums.length;
+//   let result = 0;
+//   for (let i = 0; i < n; i += 1) { // start index of subarray
+//     let sum = 0;
+//     for (let j = i; j < n; j += 1) { // end index of subarray (include!!!)
+//       sum += nums[j];
+//       if (sum === k) result += 1;
+//     }
+//   }
 
-  return result;
-};
+//   return result;
+// };
 
 /**
  * Reduce Brute Force(PrefixSum) + 2 Sum(HashMap)
- * 
+ * Whenever the sums has increased by a value of k, we've found a subarray of sums=k.
+ * Store how many remainder(prefix_sum[i]-k) occured in nums in memo.
+ * Each prefix_sum[i] can be future remainder.
  * T: O(n)
  * S: O(n)
  * Runtime: 96 ms
  */
 const subarraySum = (nums, k) => {
-  if (nums === undefined || nums.length === 0 || k === undefined) return 0;
+  if (nums.length === 0) return 0;
 
-  // key: sum, value: count, (Put sum in map, check sum-k in map)
-  let map = {
-    0: 1 // if sum == k, sum-k == 0, we can find one match by default here
+  // key: remainder, value: count, (Put remainder in memo, check prefix_sum[i]-k in memo)
+  let memo = {
+    0: 1 // if prefix_sum == k, prefix_sum-k == 0, add <remainder:0, count:1> to include thise valid case.
   }; 
 
-  let sum = 0;
+  let remainder = 0;
   let result = 0;
-  for (let num of nums) {
-    sum += num;
-    // Find match, Update result
-    if ((sum - k) in map) result += map[sum - k];
-    // Old sum, Update map
-    if (sum in map) map[sum] += 1;
-    // New sum (sum not in map), Update map
-    else map[sum] = 1;
-  }
+  for (const num of nums) {
+    remainder += num;
+    if ((remainder - k) in memo) result += memo[remainder - k]; // Find match, Update result
 
+    if (remainder in memo) memo[remainder] += 1; // Old remainder, Update memo
+    else memo[remainder] = 1; // New remainder (remainder not in memo), Update memo
+  }
   return result;
 };
 
 console.log(subarraySum([1,1,1], 2)); // 2
+console.log(subarraySum([1, -1, 2, -1, 1, 1], 2)); // 6 
+
+/**
+ * Why can't be solved with 2 pointer/sliding window?
+ * By the time your right pointer moves to an index j, you should guarantee that there are no other solutions 
+ * found in from left pointer i < j -1 to right pointer j - 1. 
+ * However, if the array contains negatives, this case will happen.
+ */
