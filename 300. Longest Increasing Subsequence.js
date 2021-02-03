d32960bf05b6f4c92d328a429ced00012f6223b3
Bruce Weng <weng8916@gmail.com>
2021-02-02 23:30:37 -0800

    300. lowerbound solution O(nlogn)

--- 300. Longest Increasing Subsequence.js
+++ 300. Longest Increasing Subsequence.js
@@ -27,24 +27,17 @@ Follow up: Could you improve it to O(n log n) time complexity?
 /**
  * @param {number[]} nums
  * @return {number}
- */
-const lengthOfLIS = function (nums) {
-    let stages = new Array(nums.length).fill(1);
-
-    for (let i = 0; i < nums.length; i += 1) {
-        for (let j = 0; j < i; j += 1) {
-            if (nums[i] > nums[j]) {
-                stages[i] = Math.max(stages[i], stages[j] + 1);
-            }
-        }
+//  */
+const lengthOfLIS = nums => {
+  let stages = new Array(nums.length).fill(1);
+  for (let end = 0; end < nums.length; end += 1) {
+    for (let start = 0; start < end; start += 1) {
+      if (nums[end] > nums[start]) {
+        stages[end] = Math.max(stages[end], stages[start] + 1);
+      }
     }
-
-    let result = 0;
-    for (let states of stages) {
-        result = Math.max(result, states);
-    }
-
-    return result;
+  }
+  return Math.max(...stages);
 };
 
 /**
@@ -65,6 +58,37 @@ const lengthOfLIS = function (nums) {
  * https://leetcode.com/problems/longest-increasing-subsequence/discuss/74824/JavaPython-Binary-search-O(nlogn)-time-with-explanation
  * https://www.cs.princeton.edu/courses/archive/spring13/cos423/lectures/LongestIncreasingSubsequence.pdf
  * 
+ * 1. Let result array stores smallest last number in cell i.
+ * 2. Cell i represent as smallest last number in length i.
+ * 3. For each num, search for the smallest number in result that greater than num, result[i] = num.
+ * 4. return result.length as the length of longest increawsing subsequence.
  * T: O(nlogn)
  * S: O(n)
  */
+function lengthOfLIS(nums) {
+  const result = [];
+  for (const num of nums) {
+    const insert_index = lowerBound(result, num);
+    if (insert_index === result.length) result.push(num);
+    else result[insert_index] = num; 
+  }
+  return result.length;
+}
+
+function lowerBound(A, T) {
+  let N = A.length,
+      start = 0,
+      end = N;
+  while (start < end) {
+    let mid = Math.floor(start + (end - start) / 2);
+    if (A[mid] < T)
+      start = mid + 1;
+    else
+      end = mid;
+  }
+  return start;
+};
+
+console.log(lengthOfLIS([10,9,2,5,3,7,101,18])); // 4
+console.log(lengthOfLIS([0,1,0,3,2,3])); // 4
+console.log(lengthOfLIS([7,7,7,7,7,7,7])); // 1
