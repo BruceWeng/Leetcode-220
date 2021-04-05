85d67b928aed2c36c4b68890894c53194e68ae0c
Bruce Weng <weng8916@gmail.com>
2021-04-05 16:34:14 -0700

    21. Merge Two Sorted Lists remove else

--- 21. Merge Two Sorted Lists.js
+++ 21. Merge Two Sorted Lists.js
@@ -20,31 +20,25 @@ Output: 1->1->2->3->4->4
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    let dummy = new ListNode(0);
-    let current = dummy;
+  let dummy = new ListNode(0);
+  let current = dummy;
 
-    while (l1 !== null && l2 !== null) {
-        if (l1.val < l2.val) {
-            current.next = l1;
-            l1 = l1.next;
-        } else {
-            current.next = l2;
-            l2 = l2.next;
-        }
-        current = current.next;
-    }
-
-    // Merge after one list finished traversing
-    while (l1 !== null || l2 !== null) {
-        if (l1 !== null) {
-            current.next = l1;
-            l1 = l1.next;
-        } else {
-            current.next = l2;
-            l2 = l2.next;
-        }
-        current = current.next;
-    }
-
-    return dummy.next;
+  while (l1 !== null && l2 !== null) {
+    [l1, l2, current.next, current] = nextValue1(l1, l2)
+  }
+  // Merge after one list finished traversing
+  while (l1 !== null || l2 !== null) {
+    [l1, l2, current.next, current] = nextValue2(l1, l2)
+  }
+  return dummy.next;
 };
+
+function nextValue1(l1, l2) {
+  if (l1.val < l2.val) return [l1.next, l2, l1, l1]
+  return [l1, l2.next, l2, l2]
+}
+
+function nextValue2(l1, l2) {
+  if (l1 !== null) return [l1.next, l2, l1, l1]
+  return [l1, l2.next, l2, l2]
+}
