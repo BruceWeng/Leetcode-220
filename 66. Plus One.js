88022ff0ed6ca8ec4d8d7a97768163b58e7c047b
Bruce Weng <weng8916@gmail.com>
2021-04-05 20:30:15 -0700

    66. Plus One remove else

--- 66. Plus One.js
+++ 66. Plus One.js
@@ -32,9 +32,8 @@ var plusOne = function(digits) {
     if (digits[i] < 9) {
       digits[i] += 1;
       return digits;
-    } else {
-      digits[i] = 0;
     }
+    digits[i] = 0;
   }
   
   digits = [1, ...digits];
