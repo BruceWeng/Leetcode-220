c92e6b269886be5ba673793c296a8ce3843b968a
Bruce Weng <weng8916@gmail.com>
2022-01-17 17:14:19 -0800

    55. Jump Game

--- 55. Jump Game.js
+++ 55. Jump Game.js
@@ -9,12 +9,55 @@ console.log(canJump(nums1)) // true
 const nums2 = [3,2,1,0,4]
 console.log(canJump(nums2)) // false
 
+/**
+Algorithm1:
+Bottom Up Brute Force:
+0. Declare reach(bool[n]) to store if the current index can be reached from
+previous indexes
+
+1. Declare two pointers, i = 0 and j = 1, use two loops to validate indicate all
+the possible, steps before A[i] that can be reach from j. (reach[i+j] = ture)
+
+2. return reach[n-1]
+
+T: O(n^2)
+S: O(n)
+Input: [2, 3, 1, 1, 4]
+
+i = 0, j = 1 -> 2:
+[2, 3, 1, 1, 4]
+ ^
+ i
+reach: [true, true, true, false, false]
+                ^     ^
+i = 1, j = 1 -> 3:
+[2, 3, 1, 1, 4]
+    ^
+    i
+reach = [true, true, true, true, true]
+                       ^     ^     ^
+
+Algorithm2:
+Buttom Up Memorization:
+Modify Algo1, the reach array must be a continuous true array, store only the farest index
+1. Declare k as the farest index
+2. k = max(k, i + j), since k stores the max index and max index comes from i + A[i]:
+k = max(k, i + A[i])
+
+T: O(n)
+S: O(1)
+
+ * @param {number[]} nums 
+ * @returns {boolean}
+ */
 function canJump(nums) {
-  const dp = Array(nums.length).fill(false) // dp[i] stores if it's possible to reach from first index
-  dp[0] = true
+  if(nums === undefined || nums.length === 0) return false;
+  
+  let k = 0;
+  
   for(let i=0; i<nums.length; i++) {
-    if(dp[i]===true && i+nums[i]>=dp.length-1) return true
-    if(dp[i]===true) for(let j=1; j<=nums[i]; j++) dp[i+j] = true
+    if(i <= k) k = Math.max(k, i+nums[i]);
   }
-  return dp[dp.length-1]
+  
+  return k >= nums.length-1;
 }
